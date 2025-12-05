9d3be0098d521149c1d0a18ede9eeeb59ec2c94f
rajuray143 <kumar.raju.bscit@gmail.com>
2025-12-05 07:29:12 +0530

    changes in file

--- src/main/java/com/raju/demojenkingsdemo/Controller.java
+++ src/main/java/com/raju/demojenkingsdemo/Controller.java
@@ -1,5 +1,6 @@
 package com.raju.demojenkingsdemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class Controller {
 
+    @GetMapping("/hello")
     public String getUserDetails(){
         return "hello raju";
     }
